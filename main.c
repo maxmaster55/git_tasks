125574c687ac6a2949c3864776b2d3b0e9a83065
maxmaster <yousseflkashef000@gmail.com>
2025-10-23 17:20:50 +0300

    added argument checking and a, b to use in the functions

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]) {
+
+    // check: got two arguments
+    if (argc != 3) {
+        printf("please enter 2 arguments to do calculation\n");
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+
 
     return 0;
 }
